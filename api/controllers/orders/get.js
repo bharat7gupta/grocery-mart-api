f7a2ffb0b95d698ed85dc62944b4a80fa6701cb6
Bharat Gupta <bharat.gupta@flipkart.com>
2020-12-06 21:23:57 +0530

    updated my orders api to include product details

--- api/controllers/orders/get.js
+++ api/controllers/orders/get.js
@@ -20,9 +20,46 @@ module.exports = {
       const userId = decodedData.id;
       const orders = await Order.find({ userId });
 
+      const productIds = orders.reduce((acc, curr) => {
+        const orderProductIds = curr.products.map(p => p.productId);
+
+        for (let i=0; i<orderProductIds.length; i++) {
+          if (!acc.find(id => id === orderProductIds[i])) {
+            acc.push(orderProductIds[i]);
+          }
+        }
+
+        return acc;
+      }, []);
+
+      const projetion = sails.config.custom.productProjection;
+      const allowedFields = Object.keys(sails.config.custom.productProjection).filter(t => !!projetion[t]);
+
+      const products = await Product.find({
+        where: { productId : productIds },
+        select: allowedFields
+      });
+
+      const orderWithProductDetails = orders.map(o => {
+        const productsWithDetails = o.products.map(p => {
+          const product = products.find(pr => pr.productId === p.productId);
+          const { id, buyingOptions, preferences, ...restProps } = product;
+          const buyingOption = buyingOptions.find(bo => bo.unit === p.unit);
+          const { inventory, ...restBuyingOption } = buyingOption;
+
+          return {
+            ...restProps,
+            buyingOption: restBuyingOption,
+            quantity: p.quantity,
+          };
+        });
+
+        return { ...o, products: productsWithDetails };
+      });
+
       this.res.json({
         code: 'success',
-        data: orders
+        data: orderWithProductDetails
       })
     } catch (e) {
       this.res.json({
